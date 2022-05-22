6a3fa9a9b9d213fdff4f2cb24b79c27f2c605189
jordan-barrett-jm <jordan.barrett.jm@gmail.com>
2022-05-22 08:58:19 -0500

    Added CSV with country codes, added function to get country capital and ISO numeric code

--- generate-report.py
+++ generate-report.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pandas as pd
 
 #this function accepts a country's two-letter ISO code and 
 #returns indicator data from the World Bank Indicators API
@@ -13,21 +14,42 @@ def fetchWBIndicatorData(countryCode):
         {"name":"Total Exports", "WB_name": "NE.EXP.GNFS.CD", "yearlyData": []},
         {"name":"Total Imports", "WB_name": "NE.IMP.GNFS.CD", "yearlyData": []},
         {"name":"Trade (% of GDP)", "WB_name": "NE.TRD.GNFS.ZS", "yearlyData": []},
-        {"name":"Population", "WB_name": "SP.POP.TOTL", "yearlyData": []}
+        {"name":"Population", "WB_name": "SP.POP.TOTL", "yearlyData": []},
+        {"name":"Land Area (sq. km)", "WB_name": "AG.LND.TOTL.K2", "yearlyData": []},
+        {"name":"Arable Land (% land area)", "WB_name": "AG.LND.ARBL.ZS", "yearlyData": []}
     ]
     #populate the yearlyData for each indicator
     for i in range(len(indicators)):
-        code = indicators[i]["WB_name"]
-        req_URL = f"https://api.worldbank.org/v2/country/{countryCode}/indicator/{code}?format=json"
+        indicator_code = indicators[i]["WB_name"]
+        req_URL = f"https://api.worldbank.org/v2/country/{countryCode}/indicator/{indicator_code}?format=json"
         try:
             res = requests.get(req_URL).json()
             #put data for the last 10 years in a (year, value) tuple
-            yearly_data = [(val["date"], val["value"]) for val in res[1][:11]]
+            yearly_data = [(val["date"], val["value"]) for val in res[1][:16]]
             indicators[i]["yearlyData"] = yearly_data
         except Exception as e:
             print (e)
     return indicators
 
-indicator = fetchWBIndicatorData("JM")
-print (indicator)
+#fetches the capital of the given country using its
+#2-letter ISO country code from the WB Country API
+def fetchWBCapital(countryCode):
+    req_URL  = f"https://api.worldbank.org/v2/country/{countryCode}?format=json"
+    try:
+        res = requests.get(req_URL).json()
+        capital = res[0]['capitalCity']
+    except Exception as e:
+        print (e)
+    return capital
+
+#get ISO numeric country code from ISO 2-letter country code
+def getNumericCode(countryCode):
+    codes = pd.read_csv("country_codes.csv")
+    try:
+        numeric_code = codes[codes['Alpha-2 code'] == countryCode]['Numeric'].values[0]
+    except Exception as e:
+        print (e)
+    return numeric_code
+
+getNumericCode("JM")
 
